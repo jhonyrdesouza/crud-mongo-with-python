dbcc98d18029812becbdf9a188ca57987d8d3548
jhony.souza <jhony.souza@tegrus.com.br>
2022-11-28 02:13:34 -0300

    fix: update splash screen

--- src/utils/splash_screen.py
+++ src/utils/splash_screen.py
@@ -1,10 +1,11 @@
 from utils import config
 
+
 class SplashScreen:
 
     def __init__(self):
         # Nome(s) do(s) criador(es)
-        self.created_by = "Howard Roatti"
+        self.created_by = "Cleverton Liltk, Gustavo Christ, Jhony Souza, Lucio Ewald e Wellington da Silva"
         self.professor = "Prof. M.Sc. Howard Roatti"
         self.disciplina = "Banco de Dados"
         self.semestre = "2022/2"
@@ -15,22 +16,19 @@ class SplashScreen:
         return df[f"total_{collection_name}"].values[0]
 
     def get_updated_screen(self):
-        return f"""
-        ########################################################
-        #                   SISTEMA DE VENDAS                     
-        #                                                         
-        #  TOTAL DE REGISTROS:                                    
-        #      1 - PRODUTOS:         {str(self.get_documents_count(collection_name="produtos")).rjust(5)}
-        #      2 - CLIENTES:         {str(self.get_documents_count(collection_name="clientes")).rjust(5)}
-        #      3 - FORNECEDORES:     {str(self.get_documents_count(collection_name="fornecedores")).rjust(5)}
-        #      4 - PEDIDOS:          {str(self.get_documents_count(collection_name="pedidos")).rjust(5)}
-        #      5 - ITENS DE PEDIDOS: {str(self.get_documents_count(collection_name="itens_pedido")).rjust(5)}
-        #
-        #  CRIADO POR: {self.created_by}
-        #
-        #  PROFESSOR:  {self.professor}
-        #
-        #  DISCIPLINA: {self.disciplina}
-        #              {self.semestre}
-        ########################################################
-        """
+        print(f"""
++------------------------------------------------------------------------------------------+
+|  # SISTEMA DE CONTROLE DE FATURAS:
+|
+|  TOTAL DE REGISTROS:
+|  1 - Clientes: {str(self.get_documents_count(collection_name="clientes")).rjust(5)}
+|  2 - Faturas: {str(self.get_documents_count(collection_name="fatura")).rjust(5)}
+|
+|  DESENVOLVIDO POR:
+|  {self.created_by}
+|
+|  PROFESSOR: {self.professor}
+|
+|  DISCIPLINA: {self.disciplina} | {self.semestre}
++------------------------------------------------------------------------------------------+
+""")
