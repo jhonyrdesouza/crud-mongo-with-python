29ad3c98ce55222433b357c7bb6937876044e870
jhony.souza <jhony.souza@tegrus.com.br>
2022-11-28 02:15:16 -0300

    feat: create app

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,48 +1,39 @@
 MENU_PRINCIPAL = """Menu Principal
-1 - Relatórios
-2 - Inserir Registros
-3 - Atualizar Registros
-4 - Remover Registros
-5 - Sair
+1 - Relatórios:
+2 - Inserir Registros:
+3 - Atualizar Registros:
+4 - Remover Registros:
+5 - Sair?
 """
 
 MENU_RELATORIOS = """Relatórios
-1 - Relatório de Pedidos por Fornecedores
-2 - Relatório de Pedidos
-3 - Relatório de Produtos
-4 - Relatório de Clientes
-5 - Relatório de Fornecedores
-6 - Relatório de Itens de Pedidos
-0 - Sair
+1 - Relatório de Clientes:
+2 - Relatório de Faturas:
+3 - Total de faturas por cliente:
+0 - Sair?
 """
 
 MENU_ENTIDADES = """Entidades
-1 - PRODUTOS
-2 - CLIENTES
-3 - FORNECEDORES
-4 - PEDIDOS
-5 - ITENS DE PEDIDOS
+1 - Clientes:
+2 - Faturas:
 """
 
-# Consulta de contagem de registros por tabela
+# Consulta de contagem de registros por tabela:
 def query_count(collection_name):
-   from conexion.mongo_queries import MongoQueries
-   import pandas as pd
+    from services.mongoQueries import MongoQueries
+    import pandas as pd
 
-   mongo = MongoQueries()
-   mongo.connect()
+    mongo = MongoQueries()
+    mongo.connect()
 
-   my_collection = mongo.db[collection_name]
-   total_documentos = my_collection.count_documents({})
-   mongo.close()
-   df = pd.DataFrame({f"total_{collection_name}": [total_documentos]})
-   return df
+    collection = mongo.db[collection_name]
+    total_documentos = collection.count_documents({})
+    mongo.close()
+    df = pd.DataFrame({f"total_{collection_name}": [total_documentos]})
+    return df
 
-def clear_console(wait_time:int=3):
-    '''
-       Esse método limpa a tela após alguns segundos
-       wait_time: argumento de entrada que indica o tempo de espera
-    '''
+
+def clear_console(wait_time: int = 5):
     import os
     from time import sleep
     sleep(wait_time)
